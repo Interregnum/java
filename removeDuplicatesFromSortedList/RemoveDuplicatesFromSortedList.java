ebebf2961a65a2e3676b2ebafd978a76aa35cf75
Interregnum <mczhong.dev@gmail.com>
2014-06-18 15:55:28 -0400

    Commit RemoveDuplicatesFromSortedList.java.

---
+++ removeDuplicatesFromSortedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,38 @@
+package removeDuplicatesFromSortedList;
+
+import common.ListNode;
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * @author Mengchao Zhong
+ */
+public class RemoveDuplicatesFromSortedList {
+
+	/**
+	 * Test Case.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+	/**
+	 * Solution.
+	 * @param head
+	 * @return
+	 */
+    public ListNode deleteDuplicates(ListNode head) {
+        if(null == head) {
+        	return null;
+        }
+    	ListNode node = head;
+    	while(null != node.next) {
+    		if(node.val == node.next.val) {
+    			node.next = node.next.next;
+    		}
+    		else {
+    			node = node.next;
+    		}
+    	}
+    	return head;
+    }
+}
