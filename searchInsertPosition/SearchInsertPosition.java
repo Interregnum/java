b0f247ad84388a8c47a44035b6fa9092f435a2e5
Interregnum <mczhong.dev@gmail.com>
2014-06-18 14:31:42 -0400

    Commit SearchInsertPosition.java.

---
+++ searchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,39 @@
+package searchInsertPosition;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ * @author Mengchao Zhong
+ */
+public class SearchInsertPosition {
+
+	/**
+	 * Test case.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] sortedArray = {-1, 2, 4, 6, 7, 8, 11, 14, 55, 123, 192, 212};
+		System.out.println(searchInsert(sortedArray, 0));
+		System.out.println(searchInsert(sortedArray, 14));
+		System.out.println(searchInsert(sortedArray, 131));
+		System.out.println(searchInsert(sortedArray, 250));
+	}
+
+	/**
+	 * Solution.
+	 * @param A
+	 * @param target
+	 * @return
+	 */
+    public static int searchInsert(int[] A, int target) {
+    	for(int i = 0; i < A.length; ++i) {
+        	if(target > A[i]) {
+        		continue;
+        	}
+        	else {
+        		return i;
+        	}
+        }
+    	return A.length;
+    }
+}
