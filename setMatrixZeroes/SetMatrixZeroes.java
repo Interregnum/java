63d0897b7d385bf4aac2b139547c8b3fe38f0cfe
Mengchao Zhong <mczhong.dev@gmail.com>
2014-07-21 23:57:49 -0400

    Add solution taking O(1) space.

--- setMatrixZeroes/SetMatrixZeroes.java
+++ setMatrixZeroes/SetMatrixZeroes.java
@@ -34,18 +34,62 @@ public class SetMatrixZeroes {
         	}
         }
         for(int i = 0; i < row.length; ++i) {
-        	if(row[i] == 1) {
-        		for(int j = 0; j < matrix[0].length; ++j) {
-        			matrix[i][j] = 0;
-        		}
-        	} 
+    		for(int j = 0; j < matrix[0].length; ++j) {
+    			if(row[i] == 1 || column[j] == 1) {
+    				matrix[i][j] = 0;
+    			}
+    		}
         }
-        for(int i = 0; i < column.length; ++i) {
-        	if(column[i] == 1) {
-        		for(int j = 0; j < matrix.length; ++j) {
-        			matrix[j][i] = 0;
-        		}
-        	} 
-        }
+    }
+    
+    /**
+     * Solution: O(1) Space.
+     * @param matrix
+     */
+    public static void setZeroes2(int[][] matrix) {
+    	if(null == matrix || matrix.length == 0 || matrix[0].length == 0) {
+    		return;
+    	}
+    	
+    	boolean firstColumn = false;
+    	boolean firstRow = false;
+    	
+    	for(int i = 0; i < matrix.length; ++i) {
+    		if(matrix[i][0] == 0) {
+    			firstColumn = true;
+    			break;
+    		}
+    	}
+    	for(int i = 0; i < matrix[0].length; ++i) {
+    		if(matrix[0][i] == 0) {
+    			firstRow = true;
+    			break;
+    		}
+    	}
+    	for(int i = 1; i < matrix.length; ++i) {
+    		for(int j = 1; j < matrix[0].length; ++j) {
+    			if(matrix[i][j] == 0) {
+    				matrix[i][0] = 0;
+    				matrix[0][j] = 0;
+    			}
+    		}
+    	}
+    	for(int i = 1; i < matrix.length; ++i) {
+    		for(int j = 1; j < matrix[0].length; ++j) {
+    			if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+    				matrix[i][j] = 0;
+    			}
+    		}
+    	}
+    	if(firstColumn) {
+    		for(int i = 0; i < matrix.length; ++i) {
+    			matrix[i][0] = 0;
+        	}
+    	}
+    	if(firstRow) {
+    		for(int i = 0; i < matrix[0].length; ++i) {
+    			matrix[0][i] = 0;
+        	}
+    	}
     }
 }
