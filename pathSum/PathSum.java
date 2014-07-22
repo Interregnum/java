ab8ec3b8f0a37bf7de74a7f91afb14939c181aac
Interregnum <mczhong.dev@gmail.com>
2014-07-22 15:07:39 -0400

    Commit PathSum.java.

---
+++ pathSum/PathSum.java
@@ -0,0 +1,42 @@
+package pathSum;
+
+import common.TreeNode;
+
+/**
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * @author Mengchao Zhong
+ */
+public class PathSum {
+
+	/**
+	 * Test case.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// No valid testing data available
+	}
+
+	private boolean flag = false;
+	/**
+	 * Solution.
+	 * @param root
+	 * @param sum
+	 * @return
+	 */
+    public boolean hasPathSum(TreeNode root, int sum) {
+    	traverse(root, sum);
+    	return flag;
+    }
+    
+    public void traverse(TreeNode root, int sum) {
+    	if(null == root) {
+    		return;
+    	}
+    	if(root.val == sum && root.left == null && root.right == null) {
+    		flag = true;
+    		return;
+    	}
+    	traverse(root.left, sum - root.val);
+    	traverse(root.right, sum - root.val);
+    }
+}
