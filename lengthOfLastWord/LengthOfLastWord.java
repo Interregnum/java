1fdd6ec794ca226294c6f556f260ed4ead254057
Mengchao Zhong <mczhong.dev@gmail.com>
2014-07-28 23:43:11 -0400

    Add solution without using native String method.

--- lengthOfLastWord/LengthOfLastWord.java
+++ lengthOfLastWord/LengthOfLastWord.java
@@ -12,8 +12,8 @@ public class LengthOfLastWord {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-			System.out.println(lengthOfLastWord("Hello World"));
-			System.out.println(lengthOfLastWord(" "));
+			System.out.println(lengthOfLastWord2("Hello World"));
+			System.out.println(lengthOfLastWord2("a "));
 	}
 
 	/**
@@ -24,5 +24,24 @@ public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
         String[] words = s.split(" ");
         return words.length == 0 ? 0 : words[words.length - 1].length();
+    }
+    
+    /**
+     * Solution: Not Using String.split().
+     * @param s
+     * @return
+     */
+    public static int lengthOfLastWord2(String s) {
+    	int count = 0;
+    	for(int i = s.length() - 1; i >= 0; --i) {
+    		if(s.charAt(i) == ' ') {
+    			if(count > 0) {
+    				break;
+    			}
+    			continue;
+    		}
+    		count++;
+    	}
+    	return count;
     }
 }
