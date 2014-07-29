ab0919859fedd3bcaaecb803ceb120f723cf55cf
Mengchao Zhong <mczhong.dev@gmail.com>
2014-07-29 00:07:39 -0400

    Commit incomplete solution.

---
+++ trappingRainWater/TrappingRainWater.java
@@ -0,0 +1,27 @@
+package trappingRainWater;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ * @author Mengchao Zhong
+ */
+public class TrappingRainWater {
+
+	/**
+	 * Test case.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] array1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+		System.out.println(trap(array1));
+	}
+
+	/**
+	 * Solution.
+	 * @param A
+	 * @return
+	 */
+    public static int trap(int[] A) {
+        int totalVolume = 0;
+    	return totalVolume;
+    }
+}
