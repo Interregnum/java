1a3541947989947cbb03f062f27f7566e9d728fe
Mengchao Zhong <mczhong.dev@gmail.com>
2014-08-06 22:49:24 -0400

    Commit ValidParentheses.java.

---
+++ validParentheses/ValidParentheses.java
@@ -0,0 +1,67 @@
+package validParentheses;
+
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * @author Mengchao Zhong
+ */
+public class ValidParentheses {
+
+	/**
+	 * Test case.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(isValid("((])[]{}"));
+	}
+
+	/**
+	 * Solution.
+	 * @param s
+	 * @return
+	 */
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); ++i) {
+        	if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+        		stack.push(s.charAt(i));
+        	}
+        	else {
+        		if(stack.isEmpty()) {
+        			return false;
+        		}
+        		char cur = stack.peek();
+        		switch(s.charAt(i)) {
+        			case ')':
+        				if(cur != '(') {
+        					return false;
+        				}
+        				else {
+        					stack.pop();
+        				}
+        				break;
+        			case ']': 
+        				if(cur != '[') {
+        					return false;
+        				}
+        				else {
+        					stack.pop();
+        				}
+        				break;
+        			case '}': 
+        				if(cur != '{') {
+        					return false;
+        				}
+        				else {
+        					stack.pop();
+        				}
+        				break;
+        			default:
+        				break;	
+        		}
+        	}
+        }
+        return stack.isEmpty();
+    }
+}
