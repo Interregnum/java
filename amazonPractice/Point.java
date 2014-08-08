7dad51135e2e798b6be8c3d7769788eedbe12f67
Interregnum <mczhong.dev@gmail.com>
2014-08-08 10:33:57 -0400

    Add comment.

--- amazonPractice/Point.java
+++ amazonPractice/Point.java
@@ -1,19 +1,34 @@
 package amazonPractice;
 
+/**
+ * Point.
+ * @author Mengchao Zhong
+ */
 public class Point implements Comparable<Point> {
 	int x;
 	int y;
 
+	/**
+	 * Constructor.
+	 * @param x
+	 * @param y
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Override compareTo Method to compare distance.
+	 */
 	@Override
 	public int compareTo(Point o) {
 		return this.x * this.x + this.y * this.y - (o.x * o.x + o.y * o.y);
 	}
 	
+	/**
+	 * Override toString Method for better testing.
+	 */
 	@Override
 	public String toString() {
 		return "(" + this.x + ", " + this.y + ")";
